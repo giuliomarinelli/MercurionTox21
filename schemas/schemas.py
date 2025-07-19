9311195e9f8767efdcea4d90ec9a06babdf9a6ec
giuliomarinelli <giulio.marinelli@icloud.com>
2025-07-19 23:09:53 +0200

    removed old comment

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel, ValidationError
 
 class InferenceRequest(BaseModel):
     smiles: str
-    accessToken: str  # opzionale se vuoi già gestirlo qui
+    accessToken: str 
